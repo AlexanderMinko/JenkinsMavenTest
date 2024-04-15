38a401da36f81bc39c6501debe5ed99558c3bb16
Oleksandr Minko <oleksandr.minko@cengage.com>
2024-04-15 12:23:51 +0200

    added file ops

--- src/main/java/com/futuretech/Main.java
+++ src/main/java/com/futuretech/Main.java
@@ -1,9 +1,13 @@
 package com.futuretech;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println(Arrays.toString(args));
         var props = System.getProperties();
         props.forEach((key, value) -> {
@@ -13,5 +17,8 @@ public class Main {
         });
         System.out.println("Hello world!");
         System.out.println("Hello again!");
+        var path = Paths.get("/var/jenkins_home/what"); //var/jenkins_home
+//        Files.createFile(path);
+        Files.writeString(path, "test");
     }
 }
