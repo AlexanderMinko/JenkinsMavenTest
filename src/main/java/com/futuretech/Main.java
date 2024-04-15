a077e9141ffd906cc47e419a53ff063c7097d1f1
Oleksandr Minko <oleksandr.minko@cengage.com>
2024-04-15 11:59:59 +0200

    added props

--- src/main/java/com/futuretech/Main.java
+++ src/main/java/com/futuretech/Main.java
@@ -2,6 +2,8 @@ package com.futuretech;
 
 public class Main {
     public static void main(String[] args) {
+        var props = System.getProperties();
+        props.forEach((key, value) -> System.out.println(key + "=" + value));
         System.out.println("Hello world!");
         System.out.println("Hello again!");
     }
