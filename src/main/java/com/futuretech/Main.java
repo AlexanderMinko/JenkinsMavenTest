cb9606f76bc88f8b119023a3f54ef7b8d48c7760
Oleksandr Minko <oleksandr.minko@cengage.com>
2024-04-15 12:09:43 +0200

    added args

--- src/main/java/com/futuretech/Main.java
+++ src/main/java/com/futuretech/Main.java
@@ -1,7 +1,10 @@
 package com.futuretech;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println(Arrays.toString(args));
         var props = System.getProperties();
         props.forEach((key, value) -> {
             if ("someproperty".equals(key)) {
