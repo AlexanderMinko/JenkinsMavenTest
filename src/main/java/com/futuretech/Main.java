a06e255425a6c1e9963831be76df257b10f856f9
Oleksandr Minko <oleksandr.minko@cengage.com>
2024-04-15 12:04:54 +0200

    changed props

--- src/main/java/com/futuretech/Main.java
+++ src/main/java/com/futuretech/Main.java
@@ -3,7 +3,11 @@ package com.futuretech;
 public class Main {
     public static void main(String[] args) {
         var props = System.getProperties();
-        props.forEach((key, value) -> System.out.println(key + "=" + value));
+        props.forEach((key, value) -> {
+            if ("someproperty".equals(key)) {
+                System.out.println(key + "=" + value);
+            }
+        });
         System.out.println("Hello world!");
         System.out.println("Hello again!");
     }
